a34918726eed0d5a70c03e1ec3a3560ad8f6f08d
piljun kim <kpj091441@gmail.com>
2024-06-01 13:41:17 +0900

    Enemy Update

---
+++ MyUnityProject/Assets/Script/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathfinder;
+    Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag ("Player").transform;
+        pathfinder.baseOffset = 1.0f; // position 의 Y값이 계속 0으로 바뀌어서 추가하였습니다.
+
+        StartCoroutine (UpdatePath ());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator UpdatePath(){
+        float refreshRate = 0.25f;
+
+        while(target != null){
+            Vector3 targetPosition = new Vector3(target.position.x,0,target.position.z);
+            pathfinder.SetDestination (targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
