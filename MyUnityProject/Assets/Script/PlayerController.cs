6a91eaffa67ca9be75d3d07b75d9d61ce824db62
piljun kim <kpj091441@gmail.com>
2024-06-01 09:10:57 +0900

    Player Update

--- MyUnityProject/Assets/Script/PlayerController.cs
+++ MyUnityProject/Assets/Script/PlayerController.cs
@@ -16,7 +16,11 @@ public class PlayerController : MonoBehaviour
     {
         velocity = _velocity;
     }
-
+    public void LootAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt (heightCorrectedPoint);
+    }
     public void FixedUpdate()
     {
         myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
